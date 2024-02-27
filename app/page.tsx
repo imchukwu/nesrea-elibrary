cb0d4e1358c5936b4eac892b400d6962892e7559
Michael Chukwu <imchukwu@gmail.com>
2024-02-27 07:08:33 +0100

    key addition

--- app/page.tsx
+++ app/page.tsx
@@ -12,9 +12,9 @@ export default function Home() {
           <HomeBanner />
         </div>
         <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-          {products.map((product: any) => {
-            return <ProductCard data={product} />;
-          })}
+          {products.map((product: any, index) => (
+            <ProductCard key={product.id || index} data={product} />
+          ))}
         </div>
       </Container>
     </div>
